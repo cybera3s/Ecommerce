7adcbce1b823b0cc910a1848da064db028f43e61
ario <cybera.3s@gmail.com>
2022-02-14 18:40:10 +0330

    add CartItem > orders.models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
+from core.models import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class CartItem(BaseModel):
+    """
+        A class used to implement cart items
+    """
+
+    count = models.PositiveIntegerField(default=1)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    product = models.OneToOneField('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.count} of {self.product}'
