2e162790ece2ef092f24d7eee1fffad8dd0b6f74
ario <cybera.3s@gmail.com>
2022-02-15 00:03:12 +0330

    register custom ProductAdmin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Brand, Category, Discount
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category')
+    search_fields = ('name', 'category', 'brand')
+    list_filter = ('inventory',)
+    raw_id_fields = ('brand', 'discount', 'category')
+    fieldsets = (
+        ('information',
+         {'fields': ['name', 'price', 'description', 'brand', 'category']}),
+        ('Specifications',
+         {'fields': ['discount', 'inventory', 'slug', 'picture']})
+    )
