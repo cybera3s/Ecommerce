106c71b5754c820b78166992e6ebfed2859cff25
ario <cybera.3s@gmail.com>
2022-02-14 19:18:15 +0330

    add Product > product.models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from core.models import BaseModel
 
-# Create your models here.
+
+class Product(BaseModel):
+    """
+        A class used to implement products
+    """
+    name = models.CharField(max_length=100, verbose_name='Name')
+    price = models.PositiveIntegerField(default=0, verbose_name='Price')
+    description = models.TextField()
+    picture = models.FileField(verbose_name='product image', null=True, blank=True)
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+    discount = models.ForeignKey('Discount', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
