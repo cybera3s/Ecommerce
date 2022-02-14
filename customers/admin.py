6b021990f97300a8de62c46c73b634aaa471df98
ario <cybera.3s@gmail.com>
2022-02-14 22:57:07 +0330

    register custom AddressAdmin

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Customer, Address
 
-# Register your models here.
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('state', 'city', 'customer')
+    search_fields = ('state', 'city', 'customer')
+    list_filter = ('last_updated',)
