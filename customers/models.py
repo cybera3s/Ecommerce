891c6a2b1c71496c5a15463de1492c886451ccea
ario <cybera.3s@gmail.com>
2022-02-14 17:49:10 +0330

    add Customer > customers.models

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import BaseModel
 
-# Create your models here.
+
+class Customer(User):
+    """
+        A class used to implement customers
+    """
+    # first_name = models.CharField(max_length=32, verbose_name='First Name')
+    # last_name = models.CharField(max_length=32, verbose_name='Last Name')
+    # email = models.EmailField(verbose_name='Email')
+    phone_number = models.CharField(max_length=32, verbose_name='Phone Number')
+    gender = models.IntegerField(choices=[(1, 'male'), (2, 'female'), (3, 'other'), ])
+
