e93f4bc9110aba2bd08e07950d3d967efae15ad5
ario <cybera.3s@gmail.com>
2022-02-14 23:01:02 +0330

    register custom CustomerAdmin

--- customers/admin.py
+++ customers/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Customer, Address
 
 
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email',)
+    search_fields = ('username', 'email',)
+    list_filter = ('last_updated',)
+    list_editable = ['phone_number', ]
+
+
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
     list_display = ('state', 'city', 'customer')
