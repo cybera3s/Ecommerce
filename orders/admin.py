8d6b1dfa6ebcf2ccb7e3db5b694935e06fefcddd
ario <cybera.3s@gmail.com>
2022-02-14 23:09:02 +0330

    register custom CartAdmin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cart, OffCode, CartItem
 
-# Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'final_price',)
+    search_fields = ('customer',)
+    list_filter = ('created',)
+    raw_id_fields = ('customer', 'off_code')
