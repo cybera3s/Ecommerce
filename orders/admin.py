c6de6894443189c22c54e4e98f5e845d9da315f6
ario <cybera.3s@gmail.com>
2022-02-14 23:11:27 +0330

    register custom OffCodeAdmin

--- orders/admin.py
+++ orders/admin.py
@@ -8,3 +8,10 @@ class CartAdmin(admin.ModelAdmin):
     search_fields = ('customer',)
     list_filter = ('created',)
     raw_id_fields = ('customer', 'off_code')
+
+
+@admin.register(OffCode)
+class OffCodeAdmin(admin.ModelAdmin):
+    list_display = ('value', 'type',)
+    search_fields = ('value', 'type')
+    list_filter = ('created',)
