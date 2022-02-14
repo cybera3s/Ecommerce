b38f8fd29a47578632bbf10b1b65c85f82a0d110
ario <cybera.3s@gmail.com>
2022-02-14 20:07:51 +0330

    set related_name > orders.Cart.off_code

--- orders/models.py
+++ orders/models.py
@@ -9,7 +9,7 @@ class Cart(BaseModel):
     """
     total_price = models.PositiveIntegerField(default=0, verbose_name='Total Price')
     final_price = models.PositiveIntegerField(default=0, verbose_name='Final Price')
-    off_code = models.ForeignKey('OffCode', on_delete=models.CASCADE)
+    off_code = models.ForeignKey('OffCode', on_delete=models.CASCADE, related_name='carts')
     customers = models.ForeignKey('Customer', on_delete=models.CASCADE)
 
 
