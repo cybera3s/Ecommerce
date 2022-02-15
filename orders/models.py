4c30f869a00a0a0cf9cada05acd827112dbc0ca6
ario <cybera.3s@gmail.com>
2022-02-16 01:50:21 +0330

    add Meta class > orders.CartItem

--- orders/models.py
+++ orders/models.py
@@ -55,6 +55,9 @@ class CartItem(BaseModel):
     cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='items')
     product = models.OneToOneField(Product, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = 'orders'
+
     @classmethod
     def filter_by_category(cls, category: Category):
         """
