4e818137e0d927ea132cefe7be0f54c42513fd86
ario <cybera.3s@gmail.com>
2022-02-16 16:35:25 +0330

    include customers.urls

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('product.urls', namespace='product'))
+    path('', include('product.urls', namespace='product')),
+    path('customers/', include('customers.urls', namespace='customers')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
