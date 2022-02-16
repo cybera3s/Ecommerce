7d6972e86995e6a06d176359ee7bb2d0beef0203
ario <cybera.3s@gmail.com>
2022-02-16 16:37:13 +0330

    include orders.urls

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls', namespace='product')),
     path('customers/', include('customers.urls', namespace='customers')),
+    path('orders/', include('orders.urls', namespace='orders')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
