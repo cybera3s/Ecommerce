dffa1f88e1f045517eb578f3237a9e94d681bc79
ario <cybera.3s@gmail.com>
2022-02-16 12:56:19 +0330

    config language localization

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -112,12 +112,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 LANGUAGES = (
     ('en', 'English'),
     ('fa', 'Farsi')
 )
-LOCALE_PATHS = BASE_DIR / 'locale'
+LOCALE_PATHS = (BASE_DIR / 'locale/',)
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
