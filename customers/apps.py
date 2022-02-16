a969bdc40409327fb837be1d54171654c33c631f
ario <cybera.3s@gmail.com>
2022-02-16 13:52:59 +0330

    translate app name

--- customers/apps.py
+++ customers/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext as _
 
 
 class CustomersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'customers'
+    verbose_name = _('Customers')
