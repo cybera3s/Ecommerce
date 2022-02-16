c19ca26b870f1f1d5cd8028aff854362dd9a1f0d
ario <cybera.3s@gmail.com>
2022-02-16 15:47:16 +0330

    create LandingView > product.views

--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingView(View):
+    template_name = 'product/landing.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
