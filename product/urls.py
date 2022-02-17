0b7eacacfa4c00cbd8ea62e0d2c12a1085c38f51
ario <cybera.3s@gmail.com>
2022-02-17 18:58:02 +0330

    add product_list_create_api url > product

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from .apis import *
 
 app_name = 'product'
 
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
+    path('api/product_list/', ProductListCreateApiView.as_view(), name='product_list_create_api'),
 ]
