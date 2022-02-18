0a9f0077d664b2805ef9d727095cfcad69d4fa83
ario <cybera.3s@gmail.com>
2022-02-19 02:04:38 +0330

    refactor Customer > customers

--- customers/models.py
+++ customers/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from core.models import User
 from django.utils.translation import gettext as _
 from core.models import BaseModel
 
 
-class Customer(User):
+class Customer(models.Model):
     """
         A class used to implement customers
     """
-    # first_name = models.CharField(max_length=32, verbose_name='First Name')
-    # last_name = models.CharField(max_length=32, verbose_name='Last Name')
-    # email = models.EmailField(verbose_name='Email')
+    user = models.OneToOneField(User, on_delete=models.RESTRICT, verbose_name=_('User'))
     phone_number = models.CharField(max_length=32, verbose_name=_('Phone Number'))
     gender = models.IntegerField(choices=[(1, 'male'), (2, 'female'), (3, 'other'), ], verbose_name=_('Gender'))
 
