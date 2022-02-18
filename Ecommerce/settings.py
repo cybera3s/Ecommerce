2398093287e1fee03fb481b6abd9604c8fbc243d
ario <cybera.3s@gmail.com>
2022-02-19 02:35:45 +0330

    change lang

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'en'
 LANGUAGES = (
     ('en', 'English'),
     ('fa', 'Farsi')
