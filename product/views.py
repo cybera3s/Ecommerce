7a96441622cd820187aee81c4b15be066b4743bb
ario <cybera.3s@gmail.com>
2022-02-19 02:00:48 +0330

    refactor views > product

--- product/views.py
+++ product/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from django.utils.translation import gettext as _
 
 
 class LandingView(View):
     template_name = 'product/landing.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        data = {
+            'title': _('landing')
+        }
+        return render(request, self.template_name, data)
