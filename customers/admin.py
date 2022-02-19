882a0fe2144bc003e330712cf026e9bdfabf2c46
ario <cybera.3s@gmail.com>
2022-02-19 20:04:43 +0330

    refactor admin.py > customers

--- customers/admin.py
+++ customers/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Customer, Address
+from core.models import User
 
 
 class AddressInline(admin.StackedInline):
@@ -7,10 +9,16 @@ class AddressInline(admin.StackedInline):
     extra = 1
 
 
+UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
+UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
+UserAdmin.ordering = ('phone',)
+UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
+UserAdmin.add_fieldsets[0][1]['fields'] = ('phone', 'password1', 'password2')
+
+
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email',)
-    search_fields = ('username', 'email',)
+    list_display = ('user', 'gender')
     inlines = [AddressInline]
 
 
@@ -19,3 +27,6 @@ class AddressAdmin(admin.ModelAdmin):
     list_display = ('state', 'city', 'customer')
     search_fields = ('state', 'city', 'customer')
     list_filter = ('last_updated',)
+
+
+admin.site.register(User, UserAdmin)
