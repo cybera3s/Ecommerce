55b5fb6b794b051cd6c8dae8d28686b30e9b9532
ario <cybera.3s@gmail.com>
2022-02-21 00:24:37 +0330

    add create_user method > core.MyUserManager

--- core/models.py
+++ core/models.py
@@ -92,6 +92,10 @@ class MyUserManager(UserManager):
         username = extra_fields['phone']
         return super().create_superuser(username, email, password, **extra_fields)
 
+    def create_user(self, username=None, email=None, password=None, **extra_fields):
+        username = extra_fields['phone']
+        return super().create_user(username, email, password, **extra_fields)
+
 
 class User(AbstractUser):
     objects = MyUserManager()
