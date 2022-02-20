021c6a01b50086e7d4cd18d2585aa1bbd11545ad
ario <cybera.3s@gmail.com>
2022-02-20 19:45:40 +0330

    add post method CustomerRegisterView > customers

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views import View
+from core.models import User
 from .forms import CustomerRegistrationForm
+from .models import Customer
 
 
 class CustomerRegisterView(View):
@@ -10,10 +12,22 @@ class CustomerRegisterView(View):
     form_class = CustomerRegistrationForm
     data = {
         'title': 'Register',
+        'form': form_class
     }
 
     def get(self, request):
-
-        self.data['form'] = self.form_class
         return render(request, self.template_name, self.data)
 
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_user = User.objects.create_user(cd['phone'], cd['email'], cd['password'])
+            Customer.objects.create(user=new_user, gender=cd['gender'])
+            messages.success(request, 'Registered successfully !', 'success')
+            return redirect('product:landing')
+
+        # Handle Invalid form
+        self.data['form'] = form
+        return render(request, self.template_name, self.data)
