f0e4cbd8a6a9c299c3fe5c433ca2b1ce414a4e20
ario <cybera.3s@gmail.com>
2022-02-20 20:36:06 +0330

    add customer_login > customers.urls

--- customers/urls.py
+++ customers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomerRegisterView
+from .views import CustomerRegisterView, CustomerLoginView
 app_name = 'customers'
 
 urlpatterns = [
-    path('register/', CustomerRegisterView.as_view(), name='customer_register')
+    path('register/', CustomerRegisterView.as_view(), name='customer_register'),
+    path('login/', CustomerLoginView.as_view(), name='customer_login'),
 ]
