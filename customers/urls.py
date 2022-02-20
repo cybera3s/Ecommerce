586730278ac8a589341626d027bdd9b16ab3e400
ario <cybera.3s@gmail.com>
2022-02-20 13:40:48 +0330

    add customer_register url > customers

--- customers/urls.py
+++ customers/urls.py
@@ -1,6 +1,7 @@
-# from django.urls import path
-
+from django.urls import path
+from .views import CustomerRegisterView
 app_name = 'customers'
 
 urlpatterns = [
+    path('register/', CustomerRegisterView.as_view(), name='customer_register')
 ]
