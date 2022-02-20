359c204c06ef370968f503314d8ab3b8a8f70c42
ario <cybera.3s@gmail.com>
2022-02-20 17:48:22 +0330

    create CustomerRegisterView > customers.views

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import CustomerRegistrationForm
 
-# Create your views here.
+
+class CustomerRegisterView(View):
+    """register customer view"""
+    template_name = 'customers/register.html'
+    form_class = CustomerRegistrationForm
+    data = {
+        'title': 'Register',
+    }
+
+    def get(self, request):
+
+        self.data['form'] = self.form_class
+        return render(request, self.template_name, self.data)
+
