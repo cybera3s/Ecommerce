8edd780e497625f97fe427eb9584d1f740d37ac9
ario <cybera.3s@gmail.com>
2022-02-20 17:24:36 +0330

    refactor forms > customers

--- customers/forms.py
+++ customers/forms.py
@@ -5,17 +5,19 @@ from django import forms
 # from .models import Customer
 
 
-class CustomerRegistrationForm(forms.ModelForm):
-    GENDER_CHOICES = [('1', 'Male'), ('2', 'Female'), ('3', 'Other')]
+class CustomerRegistrationForm(forms.Form):
+    GENDER_CHOICES = [('0', 'select your gender'), ('1', 'Male'), ('2', 'Female'), ('3', 'Other')]
 
     phone = forms.CharField(max_length=100,
-                            widget=forms.TextInput(attrs={'placeholder': 'your username', 'class': 'form-control'}))
+                            widget=forms.TextInput(
+                                attrs={'class': 'form-control', 'type': 'tel', 'placeholder': 'phone'}))
     email = forms.EmailField(max_length=100,
-                             widget=forms.EmailInput(attrs={'placeholder': 'your email', 'class': 'form-control'}))
-    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+                             widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    gender = forms.ChoiceField(choices=GENDER_CHOICES,
+                               widget=forms.Select(attrs={'class': 'form-control', 'placeholder': 'Gender'}))
 
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'placeholder': 'your password', 'class': 'form-control'}))
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
     confirm_password = forms.CharField(label='confirm password',
                                        widget=forms.PasswordInput(
-                                           attrs={'placeholder': 'password confirmation', 'class': 'form-control'}))
+                                           attrs={'class': 'form-control', 'placeholder': 'confirm password'}))
