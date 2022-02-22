731bc54fab2793dd33491978e253ede9f086cbd6
ario <cybera.3s@gmail.com>
2022-02-22 16:56:55 +0330

    create CategoryListView > product

--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.utils.translation import gettext as _
+from django.views.generic import DetailView, ListView
+from product.models import Category
 
 
 class LandingView(View):
@@ -11,3 +13,13 @@ class LandingView(View):
             'title': _('landing')
         }
         return render(request, self.template_name, data)
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'product/category/category_list.html'
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        categories = Category.objects.all().exclude(root=None)
+        return categories
