78352f6ae6ff4a556ae0a49d0bfa6edd0ddc2a8a
ario <cybera.3s@gmail.com>
2022-02-22 14:41:37 +0330

    add brand_list_create_api > product

--- product/urls.py
+++ product/urls.py
@@ -7,6 +7,7 @@ app_name = 'product'
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
     path('api/product_list/', ProductListCreateApiView.as_view(), name='product_list_create_api'),
+    path('api/brand_list/', BrandListCreateApiView.as_view(), name='brand_list_create_api'),
     path('api/product/<int:pk>', ProductDetailApi.as_view(), name='product_detail_api'),
 
 ]
