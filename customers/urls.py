61e53a2a26ef50584959947ad3dfee3d9b910544
ario <cybera.3s@gmail.com>
2022-02-22 11:29:24 +0330

    add customer_logout route > customers

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CustomerRegisterView, CustomerLoginView
+from .views import CustomerRegisterView, CustomerLoginView, CustomerLogoutView
+
 app_name = 'customers'
 
 urlpatterns = [
     path('register/', CustomerRegisterView.as_view(), name='customer_register'),
     path('login/', CustomerLoginView.as_view(), name='customer_login'),
+    path('logout/', CustomerLogoutView.as_view(), name='customer_logout'),
 ]
