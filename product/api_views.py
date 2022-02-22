32225057c1b61092a030ee2d7882570481e31de3
ario <cybera.3s@gmail.com>
2022-02-22 15:19:39 +0330

    create CategoryListApiView > product

--- product/api_views.py
+++ product/api_views.py
@@ -1,9 +1,9 @@
 # from rest_framework import status, generics
 # from rest_framework.decorators import api_view
 # from rest_framework.response import Response
-from .serializers import ProductSerializer, BrandSerializer
-from product.models import Product, Brand
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from .serializers import ProductSerializer, BrandSerializer, CategorySerializer
+from product.models import Product, Brand, Category
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView, ListAPIView
 
 
 class ProductListCreateApiView(ListCreateAPIView):
@@ -19,3 +19,8 @@ class ProductDetailApi(RetrieveUpdateDestroyAPIView):
 class BrandListCreateApiView(ListCreateAPIView):
     serializer_class = BrandSerializer
     queryset = Brand.objects.all()
+
+
+class CategoryListApiView(ListAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
