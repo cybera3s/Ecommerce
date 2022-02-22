68a6835a8e6ca89abf2198dfcccdeb8e6ca3c5fb
ario <cybera.3s@gmail.com>
2022-02-22 15:12:53 +0330

    create CategorySerializer > product

--- product/serializers.py
+++ product/serializers.py
@@ -1,4 +1,4 @@
-from .models import Product, Brand
+from .models import Product, Brand, Category
 from rest_framework import serializers
 
 
@@ -19,3 +19,12 @@ class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    root = serializers.StringRelatedField()
+    discount = serializers.StringRelatedField()
+
+    class Meta:
+        model = Category
+        fields = ('name', 'root', 'discount')
