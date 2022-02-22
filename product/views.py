c984a6de860f6eb49a879ccca6961ba8c2c3b3fc
ario <cybera.3s@gmail.com>
2022-02-22 17:37:29 +0330

    add CategoryDetailView > product

--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.utils.translation import gettext as _
 from django.views.generic import DetailView, ListView
-from product.models import Category
+from product.models import Category, Product
 
 
 class LandingView(View):
@@ -23,3 +23,20 @@ class CategoryListView(ListView):
     def get_queryset(self):
         categories = Category.objects.all().exclude(root=None)
         return categories
+
+
+class CategoryDetailView(View):
+    template_name = 'product/category/category_detail.html'
+
+    def setup(self, request, *args, **kwargs):
+        self.category_instance = get_object_or_404(Category, pk=kwargs['category_id'])
+        return super().setup(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        data = {
+            'title': self.category_instance.name
+        }
+        category = self.category_instance
+        data['products'] = Product.objects.filter(category=category)
+
+        return render(request, self.template_name, data)
