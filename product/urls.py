353fa6f174c54df621230a62463b451788e8e91b
ario <cybera.3s@gmail.com>
2022-02-22 17:25:23 +0330

    add category_detail route > product

--- product/urls.py
+++ product/urls.py
@@ -6,7 +6,7 @@ app_name = 'product'
 
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
-    # path('post/<int:category_id>/', CategoryDetailView.as_view(), name='category_detail'),
+    path('category/<int:category_id>/', CategoryDetailView.as_view(), name='category_detail'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
     path('api/product_list/', ProductListCreateApiView.as_view(), name='product_list_create_api'),
     path('api/brand_list/', BrandListCreateApiView.as_view(), name='brand_list_create_api'),
