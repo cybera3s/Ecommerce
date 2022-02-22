587a3b7a26c0d6147306cdfdea69f8ed96d39ab8
ario <cybera.3s@gmail.com>
2022-02-22 14:40:20 +0330

    refactor fields > ProductSerializer

--- product/serializers.py
+++ product/serializers.py
@@ -1,8 +1,15 @@
-from .models import Product
+from .models import Product, Brand
 from rest_framework import serializers
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()
+    discount = serializers.StringRelatedField()
+    category = serializers.StringRelatedField()
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = (
+            'name', 'price', 'description', 'picture', 'inventory', 'slug', 'created', 'last_updated', 'is_active',
+            'brand',
+            'discount', 'final_worth', 'category')
