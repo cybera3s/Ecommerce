d7dc32b40b2ff9ad551fcf87a2b7a037ed53353a
ario <cybera.3s@gmail.com>
2022-02-27 17:03:55 +0330

    create api_urls.py > orders

---
+++ orders/api_urls.py
@@ -0,0 +1,7 @@
+from rest_framework import routers
+from .api_views import *
+
+router = routers.DefaultRouter()
+
+router.register('cart', CartViewSet)
+router.register('cart_item', CartItemViewSet)
