e16190df7eb098327d628f42fef279a2c13d261d
ario <cybera.3s@gmail.com>
2022-02-27 16:56:49 +0330

    add access_category_items context_proc

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -235,6 +235,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # My context_processors
+                'core.context_processors.access_category_items',
             ],
         },
     },
