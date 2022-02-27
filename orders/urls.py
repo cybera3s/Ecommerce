fa64848fbcf2ffe4f904d887cf8a4358e9834274
ario <cybera.3s@gmail.com>
2022-02-27 20:50:59 +0330

    include router urls > orders

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
-# from django.urls import path
+from django.urls import path, include
+from .api_urls import router
 
 app_name = 'orders'
 
 urlpatterns = [
+    path('', include(router.urls,),),
 ]
