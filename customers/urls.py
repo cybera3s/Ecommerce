d5942b27a80c02e1fe00aefec8fe41d873e19f1a
ario <cybera.3s@gmail.com>
2022-02-27 21:58:49 +0330

    refactor urls > customers

--- customers/urls.py
+++ customers/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import path, include
+from .api_views import *
+from .api_urls import router
 from .views import CustomerRegisterView, CustomerLoginView, CustomerLogoutView
 
 app_name = 'customers'
@@ -7,4 +9,5 @@ urlpatterns = [
     path('register/', CustomerRegisterView.as_view(), name='customer_register'),
     path('login/', CustomerLoginView.as_view(), name='customer_login'),
     path('logout/', CustomerLogoutView.as_view(), name='customer_logout'),
+    path('api/', include(router.urls)),
 ]
