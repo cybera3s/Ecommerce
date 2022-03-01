4688ea52212068c50de718f848b0025bd56720c9
ario <cybera.3s@gmail.com>
2022-03-01 14:24:24 +0330

    create BrandViewSet

--- product/api_views.py
+++ product/api_views.py
@@ -1,13 +1,8 @@
-# from rest_framework import status, generics
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView, ListAPIView
 from rest_framework import viewsets
-
-from product.models import Product, Brand, Category
-from .serializers import ProductSerializer, BrandSerializer, CategorySerializer
+from product.models import Product, Brand
+from .serializers import ProductSerializer, BrandSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -19,16 +14,10 @@ class ProductViewSet(viewsets.ModelViewSet):
     ordering_fields = '__all__'
 
 
-class ProductDetailApi(RetrieveUpdateDestroyAPIView):
-    serializer_class = ProductSerializer
-    queryset = Product.objects.all()
-
-
-class BrandListCreateApiView(ListCreateAPIView):
+class BrandViewSet(viewsets.ModelViewSet):
     serializer_class = BrandSerializer
     queryset = Brand.objects.all()
-
-
-class CategoryListApiView(ListAPIView):
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['name', 'country']
+    search_fields = ['name', 'country']
+    ordering_fields = '__all__'
