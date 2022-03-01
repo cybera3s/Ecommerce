201fd031ef1ab02a1001f7439fe7cd6330ad5f19
ario <cybera.3s@gmail.com>
2022-03-01 14:21:27 +0330

    create ProductViewSet

--- product/api_views.py
+++ product/api_views.py
@@ -1,14 +1,22 @@
 # from rest_framework import status, generics
 # from rest_framework.decorators import api_view
 # from rest_framework.response import Response
-from .serializers import ProductSerializer, BrandSerializer, CategorySerializer
-from product.models import Product, Brand, Category
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView, ListAPIView
+from rest_framework import viewsets
+
+from product.models import Product, Brand, Category
+from .serializers import ProductSerializer, BrandSerializer, CategorySerializer
 
 
-class ProductListCreateApiView(ListCreateAPIView):
+class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['category', 'discount', 'brand']
+    search_fields = ['name', ]
+    ordering_fields = '__all__'
 
 
 class ProductDetailApi(RetrieveUpdateDestroyAPIView):
