efda52905ea13c00b04e76773c6202f13a53154c
ario <cybera.3s@gmail.com>
2022-03-01 16:34:52 +0330

    add product_detail url

--- product/urls.py
+++ product/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import *
-from .api_views import *
 from .api_urls import router
+from .views import *
+
 app_name = 'product'
 
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
+    path('product/<int:pk>/<slug:product_slug>', ProductDetailView.as_view(), name='product_detail'),
     path('category/<int:category_id>/<slug:category_slug>', CategoryDetailView.as_view(), name='category_detail'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
     # product api urls
