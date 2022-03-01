1c142b9d77d4bfa761846e287df7907f89a0d979
ario <cybera.3s@gmail.com>
2022-03-01 14:19:44 +0330

    include api urls

--- product/urls.py
+++ product/urls.py
@@ -1,15 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 from .api_views import *
-
+from .api_urls import router
 app_name = 'product'
 
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
     path('category/<int:category_id>/<slug:category_slug>', CategoryDetailView.as_view(), name='category_detail'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
-    path('api/product_list/', ProductListCreateApiView.as_view(), name='product_list_create_api'),
+    # product api urls
+    path('api/', include(router.urls)),
+    # brand api urls
     path('api/brand_list/', BrandListCreateApiView.as_view(), name='brand_list_create_api'),
-    path('api/product/<int:pk>', ProductDetailApi.as_view(), name='product_detail_api'),
 
 ]
