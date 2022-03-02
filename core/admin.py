7b14b929694384c4dda26294f8ef81324bcabc13
ario <cybera.3s@gmail.com>
2022-03-03 01:22:45 +0330

    register User admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from core.models import User
 
-# Register your models here.
+UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
+UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
+UserAdmin.ordering = ('phone',)
+UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
+UserAdmin.add_fieldsets[0][1]['fields'] = ('phone', 'password1', 'password2')
+
+admin.site.register(User, UserAdmin)
