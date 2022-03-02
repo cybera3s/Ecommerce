6404df2afd40b1c9ff19ad57fda14a2276349178
ario <cybera.3s@gmail.com>
2022-03-03 01:25:48 +0330

    register AddressAdmin

--- customers/admin.py
+++ customers/admin.py
@@ -1,19 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import Customer, Address
-from core.models import User
 
 
 class AddressInline(admin.StackedInline):
     model = Address
     extra = 1
-
-
-UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
-UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
-UserAdmin.ordering = ('phone',)
-UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
-UserAdmin.add_fieldsets[0][1]['fields'] = ('phone', 'password1', 'password2')
 
 
 @admin.register(Customer)
@@ -24,9 +15,9 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
+    fields = ('state', 'city', 'postal_code', 'address_detail', 'customer')
     list_display = ('state', 'city', 'customer')
     search_fields = ('state', 'city', 'customer')
     list_filter = ('last_updated',)
 
 
-admin.site.register(User, UserAdmin)
