dcc582bac2d3dee3b14822b3797eddc126e39779
ario <cybera.3s@gmail.com>
2022-03-03 22:58:07 +0330

    set site name

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from core.models import User
@@ -7,5 +8,9 @@ UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
 UserAdmin.ordering = ('phone',)
 UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
 UserAdmin.add_fieldsets[0][1]['fields'] = ('phone', 'password1', 'password2')
+
+admin.site.site_header = settings.SITE_NAME + "Admin"
+admin.site.site_title = f"{settings.SITE_NAME} Admin Portal"
+admin.site.index_title = f"{settings.SITE_NAME}"
 
 admin.site.register(User, UserAdmin)
