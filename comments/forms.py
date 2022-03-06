7e3dce117d1d02d9f1b012ef30e9c792726fc62c
ario <cybera.3s@gmail.com>
2022-03-06 18:53:13 +0330

    complete CommentCreateForm

--- comments/forms.py
+++ comments/forms.py
@@ -7,5 +7,11 @@ class CommentCreateForm(forms.ModelForm):
         model = Comment
         fields = ['body', ]
         widgets = {
-            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'your comment here'}),
+            'body': forms.Textarea(
+                attrs={'class': 'size-110 bor8 stext-102 cl2 p-lr-20 p-tb-10', 'placeholder': 'your comment here'}),
         }
+
+    name = forms.CharField(required=True, widget=forms.TextInput(
+        attrs={'class': 'size-111 bor8 stext-102 cl2 p-lr-20', 'placeholder': 'your name'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(
+        attrs={'class': 'size-111 bor8 stext-102 cl2 p-lr-20', 'placeholder': 'your email'}))
