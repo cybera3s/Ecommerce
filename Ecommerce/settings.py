e5b78f63aaa720676ef126d4137fceff6d8c45c2
ario <cybera.3s@gmail.com>
2022-03-06 17:11:18 +0330

    install comments app

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'customers.apps.CustomersConfig',
     'orders.apps.OrdersConfig',
-    'product.apps.ProductConfig'
+    'product.apps.ProductConfig',
+    'comments.apps.CommentsConfig',
 
 ]
 MIDDLEWARE = [
