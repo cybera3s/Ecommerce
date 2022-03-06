416a6ce45f574b67f956d7aa14af81d29b9d77eb
ario <cybera.3s@gmail.com>
2022-03-06 18:54:50 +0330

    add post method > ProductDetailView

--- product/views.py
+++ product/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.utils.translation import gettext as _
 from django.views.generic import DetailView, ListView, TemplateView
+
+from comments.forms import CommentCreateForm
 from product.models import Category, Product
 
 
@@ -44,6 +48,7 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'product/product_detail.html'
     context_object_name = 'product'
+    form_class = CommentCreateForm
 
     def setup(self, request, *args, **kwargs):
         self.product_instance = get_object_or_404(Product, pk=kwargs['pk'], slug=kwargs['product_slug'])
@@ -54,5 +59,17 @@ class ProductDetailView(DetailView):
         context = self.get_context_data(object=self.object)
         comments = self.product_instance.pcomments.filter(is_reply=False, can_publish=True)
         context['comments'] = comments
-
+        context['comment_form'] = self.form_class
         return self.render_to_response(context)
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user.customer
+            request.user.first_name = form.cleaned_data['name']
+            request.user.save()
+            new_comment.product = self.product_instance
+            new_comment.save()
+            return redirect('product:product_detail', self.product_instance.id, self.product_instance.slug)
