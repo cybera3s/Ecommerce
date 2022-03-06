72b65c7ca606d4e0c83c57832d90ed76e59d571b
ario <cybera.3s@gmail.com>
2022-03-06 16:53:38 +0330

    refactor extra space

--- comments/models.py
+++ comments/models.py
@@ -9,7 +9,8 @@ class Comment(BaseModel):
     can_publish = models.BooleanField(default=False, verbose_name=_('Can Publish'))
     user = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='ucomments', verbose_name=_('User'))
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='pcomments', verbose_name=_('Product'))
-    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True, verbose_name=_('Reply'))
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True,
+                              verbose_name=_('Reply'))
     is_reply = models.BooleanField(default=False, verbose_name=_('Is Reply?'))
     body = models.TextField(max_length=400, verbose_name=_('Body'))
 
