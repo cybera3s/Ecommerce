c191e19003479091ee07cc935b74f41099f81b68
ario <cybera.3s@gmail.com>
2022-03-07 18:42:17 +0330

    insert add_to_cart url route > orders

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .api_urls import router
+from .views import AddToCartView
 
 app_name = 'orders'
 
 urlpatterns = [
     path('api/', include(router.urls,),),
+    path('add_to_cart/', AddToCartView.as_view(), name='add_to_cart')
 ]
