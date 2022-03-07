1556fac72512316550364278336ca8f4ee2760d8
ario <cybera.3s@gmail.com>
2022-03-07 20:04:34 +0330

    add cart_items context_processor > core

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,5 @@
-from product.models import Category
+import json
+from product.models import Category, Product
 
 
 def access_category_items(request):
@@ -7,3 +8,22 @@ def access_category_items(request):
     """
     categories = Category.objects.all()
     return {'nav_categories': categories}
+
+
+def cart_items(request):
+    products = []
+    total_price = 0
+    cart_items_count = 0   # for cart item count of cart icon in all pages
+
+    if 'cart' in request.COOKIES:
+
+        cart = json.loads(request.COOKIES['cart'])
+
+        for p in cart.keys():
+            product = Product.objects.get(id=p)
+            products.append((product, cart[p]))
+            total_price += int(cart[p]) * product.final_price
+
+        cart_items_count = len(cart)
+
+    return {'cart_item_count': cart_items_count, 'cart_products': products, 'cart_total_price': total_price}
