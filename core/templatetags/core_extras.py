a38d8da872e2cfbd789a3603a20b65b4feb02914
ario <cybera.3s@gmail.com>
2022-03-10 11:30:30 +0330

    add register mul filter

--- core/templatetags/core_extras.py
+++ core/templatetags/core_extras.py
@@ -2,3 +2,14 @@ from django import template
 
 register = template.Library()
 
+
+@register.filter
+def mul(value, arg):
+    try:
+        value = int(value)
+        arg = int(arg)
+        if arg: return value * arg
+    except:
+        pass
+    return ''
+
