6dd7bc80b20662f9d3d85fd15d9110f08d9cc22f
ario <cybera.3s@gmail.com>
2022-03-10 08:59:02 +0330

    add cart_checkout url route

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from .api_urls import router
 from .api_views import AddToCartApiView
+from .views import CartCheckOutView
 
 
 app_name = 'orders'
 
 urlpatterns = [
     path('api/', include(router.urls,),),
-    path('add_to_cart_api/', AddToCartApiView.as_view(), name='add_to_cart_api'),
+    path('api/cart_item/', AddToCartApiView.as_view(), name='add_to_cart_api'),
+    path('checkout/cart/', CartCheckOutView.as_view(template_name='orders/cart/cart.html'), name='cart_checkout'),
 ]
