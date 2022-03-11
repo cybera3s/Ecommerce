55067c15322227ab6b0d3504bbc7eca7ef5acc08
ario <cybera.3s@gmail.com>
2022-03-11 19:22:10 +0330

    add get_context_data method > CartCheckOutView

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from core.utils.cart import Cart
+from django.utils.translation import gettext as _
 
 
 class CartCheckOutView(TemplateView):
     template_name = 'orders/cart/cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('landing')
+        context['cart'] = Cart(self.request)
+
+        return context
