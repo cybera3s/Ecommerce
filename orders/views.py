05d9fb2e6a5cc02149c6a02e0e64daa055429f87
ario <cybera.3s@gmail.com>
2022-03-13 20:41:21 +0330

    refactor CartCheckOutView

--- orders/views.py
+++ orders/views.py
@@ -9,6 +9,5 @@ class CartCheckOutView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = _('landing')
-        context['cart'] = Cart(self.request)
 
         return context
