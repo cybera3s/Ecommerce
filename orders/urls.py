33973ef5dd7975f7344e73b01307d3f8c51097f4
ario <cybera.3s@gmail.com>
2022-03-16 19:47:07 +0330

    add cart_total_price url

--- orders/urls.py
+++ orders/urls.py
@@ -11,6 +11,7 @@ app_name = 'orders'
 urlpatterns = [
     path('api/', include(router.urls,),),
     path('api/cart_item/', CartItemApiView.as_view(), name='cart_item_api'),
+    path('api/cart_item/<pk>', CartItemApiView.as_view(), name='detail_cart_item_api'),
     path('api/cart_item/<pk>/', CartItemApiView.as_view(), name='delete_cart_item'),
 
     path('cart/checkout/', CartCheckOutView.as_view(), name='cart_checkout'),
