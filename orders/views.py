9ffe24bc7cf0bd2b371b4ce6201dbf210cc4d4e4
ario <cybera.3s@gmail.com>
2022-03-20 00:19:27 +0330

    refactor and set setup method > CartCheckOutView

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
 import time
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.shortcuts import redirect
 from django.views import View
 from django.views.generic import TemplateView
@@ -13,8 +13,12 @@ from orders.models import Cart, OffCode
 class CartCheckOutView(LoginRequiredMixin, TemplateView):
     template_name = 'orders/cart/cart.html'
 
+    def setup(self, request, *args, **kwargs):
+        self.real_cart = Cart.objects.get(customer=request.user.customer, is_active=True)
+        super().setup(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = _('Cart Checkout')
-        context['real_cart'] = Cart.objects.get(customer=self.request.user.customer, is_active=True)
+        context['real_cart'] = self.real_cart
         return context
