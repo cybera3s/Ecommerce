74f24b70ed449e62ad6296dcc9189e9ee9ed4ff1
ario <cybera.3s@gmail.com>
2022-03-20 02:18:34 +0330

    add cart_payment url

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .api_urls import router
 from .api_views import CartItemApiView
-from .views import CartCheckOutView
+from .views import CartCheckOutView, PaymentView
 
 
 app_name = 'orders'
@@ -14,4 +14,5 @@ urlpatterns = [
     path('api/cart_item/<pk>/', CartItemApiView.as_view(), name='update_cart_item'),
 
     path('cart/checkout/', CartCheckOutView.as_view(), name='cart_checkout'),
+    path('cart/payment/', PaymentView.as_view(), name='cart_payment'),
 ]
