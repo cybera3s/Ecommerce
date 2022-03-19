1ede741fd2d63b9ae467076e8a7b19e017a38aa4
ario <cybera.3s@gmail.com>
2022-03-19 11:44:06 +0330

    add real_cart context > CartCheckOutView

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,13 @@
+import time
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect
+from django.views import View
 from django.views.generic import TemplateView
 from django.utils.translation import gettext as _
+from customers.forms import AddressRegisterForm
+from customers.models import Address
+from orders.models import Cart, OffCode
 
 
 class CartCheckOutView(LoginRequiredMixin, TemplateView):
@@ -9,5 +16,5 @@ class CartCheckOutView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = _('Cart Checkout')
-
+        context['real_cart'] = Cart.objects.get(customer=self.request.user.customer, is_active=True)
         return context
