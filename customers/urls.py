ed17c87668ed8aa140ae709a7b57f28416211579
ario <cybera.3s@gmail.com>
2022-03-24 19:15:13 +0430

    add dashboard_addresses url

--- customers/urls.py
+++ customers/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path, include
 from .api_views import *
 from .api_urls import router
-from .views import CustomerRegisterView, CustomerLoginView, CustomerLogoutView, CustomerProfileView, DashboardOrdersView
+from .views import CustomerRegisterView, CustomerLoginView, CustomerLogoutView, CustomerProfileView, \
+    DashboardOrdersView, DashboardAddressView
 
 app_name = 'customers'
 
 urlpatterns = [
     path('register/', CustomerRegisterView.as_view(), name='customer_register'),
     path('login/', CustomerLoginView.as_view(), name='customer_login'),
+    path('logout/', CustomerLogoutView.as_view(), name='customer_logout'),
+
     # Dashboard URLs
     path('profile/', CustomerProfileView.as_view(), name='customer_profile'),
     path('dashboard/orders/', DashboardOrdersView.as_view(), name='dashboard_orders'),
-    path('logout/', CustomerLogoutView.as_view(), name='customer_logout'),
+    path('dashboard/addresses/', DashboardAddressView.as_view(), name='dashboard_addresses'),
+
+
     path('api/', include(router.urls)),
 ]
