2a72a913ff2e04c0e00c610878ac3b436a09d055
ario <cybera.3s@gmail.com>
2022-03-24 18:50:15 +0430

    add customer_profile url

--- customers/urls.py
+++ customers/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from .api_views import *
 from .api_urls import router
-from .views import CustomerRegisterView, CustomerLoginView, CustomerLogoutView, CustomerProfileView
+from .views import CustomerRegisterView, CustomerLoginView, CustomerLogoutView, CustomerProfileView, DashboardOrdersView
 
 app_name = 'customers'
 
 urlpatterns = [
     path('register/', CustomerRegisterView.as_view(), name='customer_register'),
     path('login/', CustomerLoginView.as_view(), name='customer_login'),
+    # Dashboard URLs
     path('profile/', CustomerProfileView.as_view(), name='customer_profile'),
     path('logout/', CustomerLogoutView.as_view(), name='customer_logout'),
     path('api/', include(router.urls)),
