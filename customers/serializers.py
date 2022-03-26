15468f60ab69ff2dd28fd809c3c43f14ff0f4cdd
ario <cybera.3s@gmail.com>
2022-03-26 14:27:59 +0430

    refactor fields > AddressSerializer

--- customers/serializers.py
+++ customers/serializers.py
@@ -4,12 +4,10 @@ from customers.models import Address, Customer
 
 
 class AddressSerializer(serializers.ModelSerializer):
-    customer = serializers.HyperlinkedRelatedField(view_name='customers:customer-detail', read_only=True)
-    url = serializers.HyperlinkedRelatedField(view_name='customers:address-detail', read_only=True)
 
     class Meta:
         model = Address
-        fields = ['url', 'customer', 'state', 'city', 'postal_code', 'address_detail']
+        fields = ['id', 'customer', 'state', 'city', 'postal_code', 'address_detail']
 
 
 class CustomerSerializer(serializers.ModelSerializer):
