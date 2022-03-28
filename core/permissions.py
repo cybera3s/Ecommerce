9d08a842e554148a0f3f93dd1437859afbde1e1b
ario <cybera.3s@gmail.com>
2022-03-28 19:41:25 +0430

    del has_object_permission method > IsSuperuserPermission

--- core/permissions.py
+++ core/permissions.py
@@ -5,9 +5,6 @@ class IsSuperuserPermission(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_superuser or request.method in SAFE_METHODS
 
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.user
-
 
 class IsOwnerPermission(BasePermission):
 
