a18639ae27e0a926725b6c3c318e39685defc8b1
ario <cybera.3s@gmail.com>
2022-03-28 18:15:36 +0430

    set permission class > CommentViewSet

--- comments/api_views.py
+++ comments/api_views.py
@@ -3,11 +3,16 @@ from rest_framework import filters
 from rest_framework import viewsets
 from .models import Comment
 from .serializers import CommentSerializer
+from rest_framework import authentication
+from rest_framework import permissions
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['product', 'is_reply', 'user']
     search_fields = ['body']
