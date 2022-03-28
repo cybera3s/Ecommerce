0de337bd9192a80e24510b499e3f549c2ddb15e4
ario <cybera.3s@gmail.com>
2022-03-28 20:19:06 +0430

    include core urls

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
-    path('rosetta/', include('rosetta.urls')),
+    path('', include('core.urls')),
+    path('', include('product.urls', namespace='product')),
     path('', include('product.urls', namespace='product')),
     path('customers/', include('customers.urls', namespace='customers')),
     path('orders/', include('orders.urls', namespace='orders')),
