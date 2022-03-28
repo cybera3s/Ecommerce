2c1804ec7fefeaa3520e3c9d43b881a3d4e44e75
ario <cybera.3s@gmail.com>
2022-03-28 22:14:57 +0430

    add Email Configuration

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -312,3 +312,11 @@ JAZZMIN_UI_TWEAKS = {
         "success": "btn-outline-success"
     }
 }
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'cybera.3s@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'gipnyhnlitrktpui'
+EMAIL_USE_TLS = True
