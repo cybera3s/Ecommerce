8a19f32eb752b8085c887de7c6f6b3555e0c5cf3
ario <cybera.3s@gmail.com>
2022-04-03 18:13:58 +0430

    add drf pagination

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -144,10 +144,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 2
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 LOGIN_URL = '/en/customers/login/'
 
