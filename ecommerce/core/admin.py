95dee42cdf63e2bd25a30d66efdc3be1eaf0d45c
ario <cybera.3s@gmail.com>
2022-05-29 11:08:37 +0430

    add SettingAdmin

--- ecommerce/core/admin.py
+++ ecommerce/core/admin.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from core.models import User
+from core.models import User, Setting
+from django_json_widget.widgets import JSONEditorWidget
+from django.db import models
+
 
 UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
 UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
@@ -14,3 +17,15 @@ admin.site.site_title = f"{settings.SITE_NAME} Admin Portal"
 admin.site.index_title = f"{settings.SITE_NAME}"
 
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(Setting)
+class SettingAdmin(admin.ModelAdmin):
+    """
+    wide Setting admin model
+    overrides default json field with a perfect editor
+    """
+    formfield_overrides = {
+        # fields.JSONField: {'widget': JSONEditorWidget}, # if django < 3.1
+        models.JSONField: {'widget': JSONEditorWidget},
+    }
